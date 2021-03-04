a0cf21ce9c97e8f11e0f1a4bd997ec851f3aea60
braguda <brian.aguda@gmail.com>
2021-03-04 15:57:33 -0800

    updated layout and generatebuttons function created

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,25 @@
 $(document).ready(() => {
     console.log("ready");
 
+    var shadyLadies = ["Nene Leakes", "Phaedra Parks", "Lisa Rinna", "RuPaul", "Teresa Guidice"];
 
+    function generateButtons() {
+        $("#header").empty();
+        for (let i = 0; i < shadyLadies.length; i++){
+            let ladyBtn = $("<button>");
+            ladyBtn.text(shadyLadies[i]);
+            ladyBtn.addClass("btn");
+            ladyBtn.addClass("btn-dark");
+            ladyBtn.attr("data-name", shadyLadies[i]);
+            $("#header").append(ladyBtn);
+        }
+    }
+
+    generateButtons();
+    
+    $("#renderBtn").click((event) => {
+        event.preventDefault();
+        console.log("clicked");
+    });
 
 });
